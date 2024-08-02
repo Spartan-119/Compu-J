323ea048ddec0d638ed80f3233279afccd196cc5
Abin (אבין ברגיס) <abinvarghese90@gmail.com>
2024-08-02 13:48:30 +0100

    updated tsa.py

--- tsa.py
+++ tsa.py
@@ -48,8 +48,8 @@ def analyze_song(file_path, song_name):
     df_seasonal = df_seasonal.asfreq('D').fillna(0)
     
     # adding this checker to make sure we have sufficient data to perform seasonal decompose
-    if len(df_seasonal) > 2:  # Adjust this threshold as needed
-        decomposition = seasonal_decompose(df_seasonal, model='additive', period=7)  # Assuming weekly seasonality
+    if len(df_seasonal) > 2:  
+        decomposition = seasonal_decompose(df_seasonal, model='additive', period=7)  
         plt.figure(figsize=(10, 6))
         decomposition.seasonal.plot(title=f'Seasonal Patterns for {song_name}', xlabel='Date', ylabel='Seasonal Component')
         plt.savefig(os.path.join(song_dir, 'seasonal_patterns.png'))
@@ -59,7 +59,7 @@ def analyze_song(file_path, song_name):
 
 # main starting point
 data_folder = 'data'
-song_numbers = list(range(1, 15)) + list(range(16, 22))  # Skipping 15
+song_numbers = list(range(1, 15)) + list(range(16, 22))  # Skipping 15 as i don't have it
 
 for i in song_numbers:
     file_name = f'song{i:02d}.csv'
